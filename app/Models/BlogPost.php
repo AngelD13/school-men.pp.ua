ac8f124fd5122bd7d43105b043d41a8c8b5c4ea4
AngelD13 <mirgorodfor@gmail.com>
2021-04-27 13:17:20 +0300

    Связь категорий и юзеров с постами

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -48,4 +48,24 @@ class BlogPost extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    /**
+     * Категории статьи
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        //Статья принадлежит категории
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    /**
+     * Автор статьи
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        //Статья принадлежит пользователю
+        return $this->belongsTo(User::class);
+    }
 }
